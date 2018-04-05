ba7d8b50055c627fb846ab61b8dae52cff9258ef
prashantsirohi04 <prashantsirohi04@gmail.com>
2018-04-05 16:06:31 +0530

    homepage and login page

---
+++ src/components/pages/HomePage.js
@@ -0,0 +1,14 @@
+import  React  from "react";
+import { Link } from "react-router-dom";
+
+const HomePage = () => (
+    <div>
+        <h1>
+            Home page
+            <Link to="/login">Login</Link>
+        </h1>
+    </div>
+);
+
+export default HomePage;
+
